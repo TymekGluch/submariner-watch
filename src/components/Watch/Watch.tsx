2aa73fd408654b5252bd0a60cd4221d60366dfc4
Tymoteusz Gluch <gluchtymoteusz@gmail.com>
2022-09-14 20:35:51 +0200

    countdown-timer-style

--- src/components/Watch/Watch.tsx
+++ src/components/Watch/Watch.tsx
@@ -2,12 +2,15 @@ import React from 'react';
 import { WatchStyled } from './Watch.styled';
 import { Case } from './Case/Case';
 import { Dial } from './Dial/Dial';
-import { CountdownTimer } from './CountdownTimer/CountdownTimer';
+import { RotatingBezel } from './RotatingBezel/RotatingBezel';
+
 
 export const Watch = () => (
     <WatchStyled>
         <Case />
+        
         <Dial />
-        <CountdownTimer />
+
+        <RotatingBezel />
     </WatchStyled>
 );  
