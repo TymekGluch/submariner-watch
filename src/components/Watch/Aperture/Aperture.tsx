1d8111a975ac97dfa74c1712acd14ab0a1a6cf22
Tymoteusz Gluch <gluchtymoteusz@gmail.com>
2022-09-22 18:20:43 +0200

    aperture-logic

--- src/components/Watch/Aperture/Aperture.tsx
+++ src/components/Watch/Aperture/Aperture.tsx
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { AperturaStyled, Number, WindowStylded } from './Aperture.styled';
 
-export const Aperture = () => (
-  <WindowStylded>
-    <AperturaStyled>
-        <Number>21</Number>
-    </AperturaStyled>
-  </WindowStylded>
-);
+export const Aperture = () => {
+  const [aperatureState, setAperatureState] = useState(3);
+
+  useEffect(() => {
+    const date = new Date();
+
+    setInterval(() => {
+      setAperatureState(date.getDate());
+    }, 1000);
+  }, []);
+
+  return (
+    <WindowStylded>
+      <AperturaStyled>
+        <Number>{aperatureState}</Number>
+      </AperturaStyled>
+    </WindowStylded>
+  );
+};
